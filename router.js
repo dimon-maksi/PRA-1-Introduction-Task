039d068d2e6089f86f8f545238a53024deb081d4
dmitriymaksimenko88@gmail.com <dmitriymaksimenko88@gmail.com>
2024-03-06 11:52:12 +0200

    Updated router

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
 import Router from 'express'
+import StudentController from "./controllers/studentController.js"
+import HomeworkController from "./controllers/homeworkController.js"
 
-const router = new Router()
+const router = new Router();
+
+router.get('/api/students', StudentController.getAll);
+router.post('/api/students', StudentController.create);
+
+router.get('/api/homeworks', HomeworkController.getAll);
+router.get('/api/homeworks/student/:id', HomeworkController.getAllByStudentId);
+router.post('/api/homeworks', HomeworkController.create);
 
 export default router;
