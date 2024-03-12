34b2a6442fb198b02f24eb5ea95375c1eac3860d
dmitriymaksimenko88@gmail.com <dmitriymaksimenko88@gmail.com>
2024-03-12 19:19:53 +0200

    Changed student model

--- models/student.js
+++ models/student.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const Student = new mongoose.Schema({
     name: {type: String, required: true},
     age: {type: Number, required: true},
-    class: {type: String, required: true}
+    class: {type: String, required: true},
+    group: {type: String, required: true}
 })
 
 export default mongoose.model('Student', Student)
